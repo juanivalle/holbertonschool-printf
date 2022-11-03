1003b183f00dd5b40fba63b814302408f1474dd5
Darian94 <dariangra@gmail.com>
2022-11-03 07:05:17 -0700

    header file

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
+int _printf(const char *format, ...);
+
+#endif 
