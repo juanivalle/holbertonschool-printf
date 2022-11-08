775cb634f8f4a67a24359f6097dd8b359413adb8
Darian94 <dariangra@gmail.com>
2022-11-08 11:01:29 -0800

    lengths differ fix

--- _printf.c
+++ _printf.c
@@ -12,6 +12,7 @@ int _printf(const char *format, ...)
 	char c;
 	char *s;
 	int i;
+	int cont = 0;
 	va_list arg;
 
 	if (format == NULL)
@@ -28,27 +29,40 @@ int _printf(const char *format, ...)
 				case 'c':
 			       		c = va_arg(arg, int);
 					_putchar(c);
+					cont++;
 					n += 2;
 					break;
 
 				case 's':
 				       	s = va_arg(arg, char*);
 					for (x = 0 ; s[x] != '\0' ; x++)
+					{
 						_putchar(s[x]);
+						cont++;
+					}
 					n += 2;
 					break;
 
 				case 'i':
 					i = va_arg(arg, int);
 					_putchar(i);
+					cont++;
 					n += 2;
+					break;
+
+				default :
+					_putchar(format[n]);
+					cont++;
 					break;
 
 			}
 		}
 		if (format[n] != '%')
+		{
 			_putchar(format[n]);
+			cont++;
+		}
 	}
 	va_end(arg);
-	return (n);
+	return (cont);
 }
